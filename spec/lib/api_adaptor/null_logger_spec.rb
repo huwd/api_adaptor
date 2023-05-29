a79240d4aff39cffccf0159870b66883ec7efc0e
Huw Diprose <mail@huwdiprose.co.uk>
2023-05-29 20:14:59 +0100

    add: NullLogger - simple logger that logs nothing
    Taken from [Karafka/null-logger][1] and mostly because [gds-api-adaptors][2]
    uses it.
    
    As that repo is now unmaintained[3], and it's simple, let's bring it into our
    maintainance scope here rather than use an unmaintained dep
    
    [1]: https://github.com/karafka/null-logger/tree/master]
    [2]: https://github.com/alphagov/gds-api-adapters/blob/8c003880b98ecd55825466d4a8e44d3019d54ea1/lib/gds_api/json_client.rb#L19
    [3]: https://github.com/karafka/null-logger/blob/8e9d86a5eea27ed5eae531ea930599d3a3433708/README.md#null-logger-unmaintained

---
+++ spec/lib/api_adaptor/null_logger_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+require 'api_adaptor/null_logger'
+
+RSpec.describe ApiAdaptor::NullLogger do
+  subject { logger }
+
+  let(:logger) { described_class.new }
+
+  it { is_expected.to respond_to(:unknown) }
+  it { is_expected.to respond_to(:fatal) }
+  it { is_expected.to respond_to(:error) }
+  it { is_expected.to respond_to(:warn) }
+  it { is_expected.to respond_to(:info) }
+  it { is_expected.to respond_to(:debug) }
+
+  it 'returns false for debug?' do
+    expect(logger).not_to be_debug
+  end
+
+  it 'returns false for info?' do
+    expect(logger).not_to be_info
+  end
+
+  it 'returns false for error?' do
+    expect(logger).not_to be_error
+  end
+
+  it 'returns false for warn?' do
+    expect(logger).not_to be_warn
+  end
+
+  it 'returns false for fatal?' do
+    expect(logger).not_to be_fatal
+  end
+end
