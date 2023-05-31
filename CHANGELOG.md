85ddb787847c1d7d618ab6c0b82a0c3821a5a7a9
Huw Diprose <mail@huwdiprose.co.uk>
2023-06-01 00:35:36 +0100

    release: 0.0.1 - Initial Release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## [Unreleased]
 
-## [0.0.0] - 2023-05-29
+## [0.0.1] - 2023-06-01
 
 - Initial release
+
+## [0.0.0] - 2023-05-29
+
+- Bootstrapping
--- lib/api_adaptor.rb
+++ lib/api_adaptor.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "api_adaptor/version"
+require_relative "api_adaptor/base"
 
 module ApiAdaptor
   class Error < StandardError; end
