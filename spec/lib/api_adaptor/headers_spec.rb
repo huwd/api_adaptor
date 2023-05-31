1237c4e11499f22f144101781fbbfa36edb1f5d7
Huw Diprose <mail@huwdiprose.co.uk>
2023-05-31 22:48:02 +0100

    add: Headers class
    Set, read and clear headers

---
+++ spec/lib/api_adaptor/headers_spec.rb
@@ -0,0 +1,30 @@
+require "api_adaptor/headers"
+
+RSpec.describe ApiAdaptor::Headers do
+  before :each do
+    Thread.current[:headers] = nil if Thread.current[:headers]
+  end
+
+  after :each do
+    ApiAdaptor::Headers.clear_headers
+  end
+
+  it "supports read/write of headers" do
+    ApiAdaptor::Headers.set_header("Accept-Language", "en-US,en;q=0.5")
+    ApiAdaptor::Headers.set_header("Content-Type", "application/pdf")
+
+    expect(ApiAdaptor::Headers.headers).to eq(
+      {
+        "Accept-Language" => "en-US,en;q=0.5",
+        "Content-Type" => "application/pdf",
+      }
+    )
+  end
+
+  it "supports clearing of headers" do
+    ApiAdaptor::Headers.set_header("Accept-Language", "en-US,en;q=0.5")
+    ApiAdaptor::Headers.clear_headers
+
+    expect(ApiAdaptor::Headers.headers).to eq({})
+  end
+end
