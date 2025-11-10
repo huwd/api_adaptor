928401215ea023aa68ba03e0e0c7705406e439e1
Huw Diprose <mail@huwdiprose.co.uk>
2025-11-10 22:45:05 +0000

    Add: Basic integration test that hits GitHub Pages URL

--- spec/api_adaptor_spec.rb
+++ spec/api_adaptor_spec.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe ApiAdaptor do
+  let(:client) { ApiAdaptor::Base.new }
+  let(:github_pages_url) { "https://huwd.github.io/api_adaptor/" }
+
+  it "can request valid JSON from a client" do
+    file_data = File.join(__dir__, "../fixtures/v1/integration/foo.json")
+    json = client.get_json("#{github_pages_ur}/foo.json")
+    expect(json.parsed_content).to eq(file_data)
+  end
 end
